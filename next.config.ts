eb62f7f649176fa65edafcca3e7519f71b365d3c
Jafar Touzeau <jafar.touzeau@gmail.com>
2026-06-27 12:07:02 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === 'production' ? '/personnal-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/personnal-website/' : '',
 };
 module.exports = nextConfig;
