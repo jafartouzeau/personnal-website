9f19f6232b59f0fe1f7b92638640621f0d07d1b0
Jafar Touzeau <jafar.touzeau@gmail.com>
2026-06-27 13:49:57 +0200

    Routing test

---
+++ src/app/musique/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+export default function Musique() {
+    return (
+        <div className={styles.page}>
+            <main>
+                <p>
+                    Vive la musique
+                </p>
+            </main>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div className={styles.intro}>
-          <h1>Bienvenue dans mon petit coin d'Internet</h1>
-        </div>
+        <p>
+          <Link href={"/musique"}>
+            musique
+          </Link>
+        </p>
       </main>
     </div>
   );
