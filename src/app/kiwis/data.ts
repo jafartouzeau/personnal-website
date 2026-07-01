86f1b51445992ade5cbd236e322bcd15a83c0d3f
Jafar Touzeau <jafar.touzeau@gmail.com>
2026-07-01 12:34:39 +0200

    Add square

--- src/app/kiwis/data.ts
+++ src/app/kiwis/data.ts
@@ -21,5 +21,25 @@ export const kiwisData: Record<string, ArtworkType[]> = {
             imageAlt: "Une glace goût kiwi.",
             imageRatio: 1752/2800
         }
+    ],
+    "square": [
+       {
+            imageTitle: "Compte sur ses doigts",
+            imageSrc:"/static/kiwis/Compter.png",
+            imageAlt: "Il s'ennuie très vite à nouveau.",
+            imageRatio: 1
+        },
+       {
+            imageTitle: "Flûte à bec",
+            imageSrc:"/static/kiwis/Flûteàbec.png",
+            imageAlt: "Dresseur de serpents.",
+            imageRatio: 1
+        },
+       {
+            imageTitle: "Lejourj",
+            imageSrc:"/static/kiwis/Lejourj.png",
+            imageAlt: "Hommage à un certain Prokofiev.",
+            imageRatio: 1
+        }
     ]
 }
