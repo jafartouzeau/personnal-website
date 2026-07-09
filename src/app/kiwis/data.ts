a3c3754692b908ab9a15d6aff0d7646096c71336
Jafar Touzeau <jafar.touzeau@gmail.com>
2026-07-09 16:30:59 +0200

    Update kiwi

--- src/app/kiwis/data.ts
+++ src/app/kiwis/data.ts
@@ -23,6 +23,12 @@ export const kiwisData: Record<string, ArtworkType[]> = {
     ],
     "formatcarre": [
        {
+            imageTitle: "Quel genre d'oiseau es-tu ?",
+            imageSrc:"/static/kiwis/quelgenredoiseau.jpg",
+            imageAlt: "Hommage à un certain Prokofiev.",
+            imageRatio: 1
+        },
+       {
             imageTitle: "Compte sur ses doigts",
             imageSrc:"/static/kiwis/ennui.jpg",
             imageAlt: "Il s'ennuie très vite à nouveau.",
@@ -35,21 +41,21 @@ export const kiwisData: Record<string, ArtworkType[]> = {
             imageRatio: 1
         },
        {
-            imageTitle: "Quel genre d'oiseau es-tu ?",
-            imageSrc:"/static/kiwis/quelgenredoiseau.jpg",
-            imageAlt: "Hommage à un certain Prokofiev.",
-            imageRatio: 1
-        },
-       {
             imageTitle: "Fessu",
             imageSrc:"/static/kiwis/fessu.jpg",
             imageAlt: "Tout le monde en a non ?",
             imageRatio: 1
         },
        {
-            imageTitle: "Ballons de bauderuche",
+            imageTitle: "Bauderuche",
             imageSrc:"/static/kiwis/baudruche.jpg",
             imageAlt: "Il vole enfin. Ce n'est peut être qu'un rêve.",
+            imageRatio: 1
+        },
+       {
+            imageTitle: "Cactus",
+            imageSrc:"/static/kiwis/cactus.jpg",
+            imageAlt: "Ça pique.",
             imageRatio: 1
         }
     ]
